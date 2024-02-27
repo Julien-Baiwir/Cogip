5e68d1547bb29f4dfbcf26c63f46f61a10da41ae
Cassidy <cassidy.rouelle@gmail.com>
2024-02-27 16:59:00 +0100

    Cass → début responsive header

--- View/includes/header.php
+++ View/includes/header.php
@@ -7,15 +7,15 @@
     <title>Cogip</title>
 </head>
 <body>
-    <header class="bg-yellow-300 flex gap-6 text-xl items-center p-10 font-display">
+    <header class="bg-yellow-300 flex gap-4 text-xl items-center p-12 font-display">
         <p class="text-5xl font-black">COGIP</p>
         <div id="navbar">
             <a href="#" class="hover:border-black hover:border-2 p-3 font-semibold">Home</a>
             <a href="#" class="hover:border-black hover:border-2 p-3 font-semibold">Invoices</a>
             <a href="#" class="hover:border-black hover:border-2 p-3 font-semibold">Compagnies</a>
-            <a href="#" class="hover:border-black hover:border-2 p-3 font-semibold">Contact</a>
+            <a href="#" class="hover:border-black hover:border-2 p-3 font-semibold">Contacts</a>
         </div>
-        <div id="log" class="ml-72">
+        <div id="log" class="lg:ml-20 xl:ml-64">
             <a href="#" class="p-3 font-semibold bg-white rounded-lg">Sign up</a>
             <a href="#" class="p-3 font-semibold">Login</a>
         </div>
