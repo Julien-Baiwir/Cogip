7e58b92a47556bc5f88057593dbf7115fa5c3409
Cassidy <cassidy.rouelle@gmail.com>
2024-02-28 13:55:18 +0100

    Cass → header

--- View/home.php
+++ View/home.php
@@ -1,3 +1,23 @@
-<?php 
-require 'View/includes/header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/output.css">
+    <title>Cogip</title>
+
+    <style>
+      #menu-toggle:checked + #navbar {
+        display: block;
+      }
+  </style>
+</head>
+<body>
+    <header class="bg-yellow-300 ">
+       <?php require 'View/includes/header.php'; ?> 
+       <section class="flex flex-col justify-center items-center font-black mt-10">
+            <h1 class="uppercase font-inter text-4xl text-center">Manage your customers and invoices easly</h1>
+            <img src="src/img/header.png" alt="" srcset="">
+        </section>
+    </header>
+</body>
--- View/includes/header.php
+++ View/includes/header.php
@@ -1,37 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./src/output.css">
-    <title>Cogip</title>
+<!-- header for all page -->
+<section class="text-xl p-8 font-display flex flex-wrap items-center lg:p-10">
+    <div class="flex-1 flex justify-between items-center"><span class="text-5xl font-bold">COGIP</span></div>
 
-    <style>
-      #menu-toggle:checked + #navbar {
-        display: block;
-      }
-  </style>
-</head>
-<body>
-    <header class="bg-yellow-300 text-xl p-8 font-display flex flex-wrap items-center lg:p-10">
-        <div class="flex-1 flex justify-between items-center"><span class="text-5xl font-black">COGIP</span></div>
+    <label for="menu-toggle" class="pointer-cursor lg:hidden block justify-self-end"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
+    <input class="hidden" type="checkbox" id="menu-toggle" />
 
-        <label for="menu-toggle" class="pointer-cursor lg:hidden block justify-self-end"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
-        <input class="hidden" type="checkbox" id="menu-toggle" />
-
-        <div id="navbar" class="hidden lg:flex lg:flex-row lg:items-center lg:gap-10 lg:w-auto w-full">
-            <nav>
-                <ul class="flex items-end flex-col lg:flex-row lg:items-center lg:justify-center font-semibold">
-                    <li class="block hover:border-black hover:border-2 p-2"><a href="#">Home</a></li>
-                    <li class="block hover:border-black hover:border-2 p-2"><a href="#">Invoices</a></li>
-                    <li class="block hover:border-black hover:border-2 p-2"><a href="#">Compagnies</a></li>
-                    <li class="block hover:border-black hover:border-2 p-2"><a href="#">Contacts</a></li>
-                </ul>
-            </nav>
-            <div class="flex items-center justify-center gap-2">
-                <a href="#" class="p-2 font-semibold bg-white rounded-lg">Sign up</a>
-                <a href="#" class="p-2 font-semibold">Login</a>
-            </div>
+    <div id="navbar" class="hidden lg:flex lg:flex-row lg:items-center lg:gap-10 lg:w-auto w-full">
+        <nav>
+            <ul class="flex items-end flex-col lg:flex-row lg:items-center lg:justify-center font-semibold">
+                <li class="block hover:border-black hover:border-2 p-2"><a href="#">Home</a></li>
+                <li class="block hover:border-black hover:border-2 p-2"><a href="#">Invoices</a></li>
+                <li class="block hover:border-black hover:border-2 p-2"><a href="#">Compagnies</a></li>
+                <li class="block hover:border-black hover:border-2 p-2"><a href="#">Contacts</a></li>
+            </ul>
+        </nav>
+        <div class="flex items-center justify-center gap-2">
+            <a href="#" class="p-2 font-semibold bg-white rounded-lg">Sign up</a>
+            <a href="#" class="p-2 font-semibold">Login</a>
         </div>
-    </header>
+    </div>
+</section>
+
 
