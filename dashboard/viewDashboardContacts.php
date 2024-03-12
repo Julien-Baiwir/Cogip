daca387206dfbd035428b6f931f0de8a1500c9bb
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-03-12 16:25:01 +0100

    dashboard

--- dashboard/viewDashboardContacts.php
+++ dashboard/viewDashboardContacts.php
@@ -1,4 +1,34 @@
-<h2>new contact</h2>
+
+<form action="index.php?url=dashboardcontacts/addContact" method="POST">
+  <h2>Add Contact</h2>
+
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" required><br><br>
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" required><br><br>
+
+  <label for="phone">Phone Number:</label>
+  <input type="tel" id="phone" name="phone" required><br><br>
+
+  <label for="company_id">Company:</label>
+  <select id="company_id" name="company_id" required>
+    <option value="">-- Select Company --</option>
+
+    <?php
+      $companies = getCompanyName(); 
+
+      if (!empty($companies)) {
+        foreach ($companies as $company) {
+          echo "<option value='" . $company['id'] . "'>" . $company['name'] . "</option>";
+        }
+      }
+    ?>
+  </select><br><br>
+
+  <input type="submit" value="Submit">
+</form>
+
 
 <?php
 // Contacts
@@ -23,3 +53,5 @@ if (!empty($fivecontacts)) {
 } else {
     echo "<p>No contacts available</p>";
 }
+
+
